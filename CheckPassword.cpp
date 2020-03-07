4fc76ca4290a83b816a8fedc40bc264bfe226e66
Celldweller <55654940+RaimeSmokyKnight@users.noreply.github.com>
2020-03-07 20:06:27 +0200

    Update CheckPassword.cpp

--- CheckPassword.cpp
+++ CheckPassword.cpp
@@ -11,20 +11,21 @@
 -наличие хотя бы одной цифры,
 -наличие хотя бы одного символа пунктуации,
 */
+
 bool CheckLengthPassword(char*);
 bool CheckSymbolPunctuation(char*);
 bool IsDigit(char*);
 bool IsBigger(char*);
 bool IsLower(char*);
 
-bool CheckPassword(char password[])
+bool CheckPassword(char* password)
 {
 	if (
-		CheckLengthPassword(password) &&
-		CheckSymbolPunctuation(password) &&
-		IsDigit(password) &&
-		IsBigger(password) &&
-		IsLower(password)
+		CheckLengthPassword(password)&&
+		(CheckSymbolPunctuation(password) ) &&
+	    ( IsDigit(password) ) &&
+		( IsBigger(password) ) &&
+		( IsLower(password) )
 		)
 	{
 		return true;
@@ -32,25 +33,23 @@ bool CheckPassword(char password[])
 	return false;
 }
 
-bool IsLower(char password[])
+bool IsLower(char* password)
 {
 	int index = 0;
-	while ((char)password[index] != '\0')
+	for (; index < strlen(password); index++)
 	{
 		if (islower(password[index]))
 			return true;
-		index++;
-	}
+    }
 	return false;
 }
 bool IsBigger(char* password)
 {
 	int index = 0;
-	while ((char)password[index] != '\0')
+	for (; index < strlen(password); index++)
 	{
 		if (!islower(password[index]))
 			return true;
-		index++;
 
 	}
 	return false;
@@ -61,42 +60,42 @@ bool CheckLengthPassword(char* password)
 		count = 0,
 		eight = 8;
 
-	while ((char)password[index] != '\0')
+	for (; index < strlen(password); index++)
 	{
 		count++;
-		index++;
 
 	}
 	return count >= eight;
+
 }
 
-bool IsDigit(char password[])
+bool IsDigit(char* password)
 {
 	int index = 0;
-	while ((char)password[index] != '\0')
+	for ( ;index < strlen(password); index++)
 	{
 		bool flag = isdigit((char)password[index]);
 		if (flag)
 			return flag;
-		index++;
-
-
 	}
+	return false;
 
 }
-bool CheckSymbolPunctuation(char password[])
+bool CheckSymbolPunctuation(char* password)
 {
 	int CountSymbolPunctuation = 6;
 	char _MasSymbolPunctuation[] = { '.', '!', ',', '?', ':', ';' };
 	int index = 0;
 	bool flag = false;
-	while ((char)password[index] != '\0')
-	{
+
+	for (size_t index = 0; index < strlen(password); index++)
+	 {
 		for (size_t i = 0; i < CountSymbolPunctuation; i++)
 		{
 			if ((char)password[index] == _MasSymbolPunctuation[i])
+			{
 				return true;
-			index++;
+			}
 
 		}
 
@@ -108,18 +107,25 @@ int main(int argc, char* argv[])
 {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-	char password[255];
+	 char password[255];
 	bool flag = false;
 	do
 	{
 		if (flag)
 		{
-			std::cout << "Пароль несоответсвует требованиям,пожалуста повторите ввод!!!\n";
+			std::cout << "Пароль несоответсвует требованиям,пожалуста убедитесь что пароль соответсвует требованиям ниже и повторите ввод!!!\n";
 		}
+		std::cout<<"Пожалуйста введите пароль который соответсвует следующим требованиям:\n"
+				"-длина пароля от 8 символов\n"
+				"-наличие хотя бы одной прописной буквы,\n"
+				"-наличие хотя бы одной строчной буквы,\n"
+				"-наличие хотя бы одной цифры,\n"
+				"-наличие хотя бы одного символа пунктуации\n";
+		
 		flag = true;
 		std::cout << "]$~Enter the password -> ";
-		std::cin >> password;
+		std::cin.getline(password,255);
 
-	} while (!CheckPassword(password));
+	} while ( CheckPassword(password)!=true);
 	return 0;
 }
